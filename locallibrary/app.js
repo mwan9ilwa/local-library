51fd1da426ba51730f75651859401758ba6c9319
Mwangilwa Zimba <mwangilwa23@gmail.com>
2022-06-13 16:55:10 +0200

    added controllers and routes

--- locallibrary/app.js
+++ locallibrary/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +42,7 @@ app.use(function(err, req, res, next) {
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://mwan9ilwa:T9mnDp3y64pJZYW@cluster0.qh6xw.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://mwan9ilwa:T9mnDp3y64pJZYW@cluster0.qh6xw.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
